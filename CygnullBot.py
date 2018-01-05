60ee555a6ecb15355e53cd43816d1deac6250489
Jeffrey Hauser <cygnull@users.noreply.github.com>
2018-01-05 09:03:41 -0500

    Add Comments
    Added various comments and descriptions.

--- CygnullBot.py
+++ CygnullBot.py
@@ -6,40 +6,38 @@ from discord.ext import commands
 import platform
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-client = Bot(description="Cygnull A.I. Discord Bot ver. 0.1.0a", command_prefix=">", pm_help = True)
+client = Bot(description="Cygnull A.I. Discord Bot ver. 0.1a", command_prefix=">", pm_help = True)
 
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
 # Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
 @client.event
 async def on_ready():
 	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
-	print('--------')
+	print('-=-=-=-=-')
 	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
-	print('--------')
+	print('-=-=-=-=-')
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
-	print('--------')
-	print('Support Discord Server: https://discord.gg/FNNNgqb')
-	print('Github Link: https://github.com/Habchy/BasicBot')
-	print('--------')
-	print('Created by Habchy#1665')
+	print('-=-=-=-=-')
+	print('Support Discord Server: https://discord.gg/hyGa7J2')
+	print('Github Link: https://github.com/cygnull/CygnullBOT')
+	print('-=-=-=-=-')
+	print('Created by Cygnull')
 
 # This is a basic example of a call and response command. You tell it do "this" and it does it.
 @client.command()
-async def ping(*args):
+async def testing(*args):
 
-	await client.say(":ping_pong: Pong!")
+	await client.say(":test_me: Test is working!")
 	await asyncio.sleep(3)
-	await client.say(":warning: This bot was created by **Habchy#1665**, it seems that you have not modified it yet. Go edit the file and try it out!")
+	await client.say(":warning: This bot was created by **Cygnull#1348**, Updates pushed to GitHub regularly!")
 # After you have modified the code, feel free to delete the line above (line 33) so it does not keep popping up everytime you initiate the ping commmand.
 	
 client.run('Mzk2MDM0NTYwNDc2ODM5OTM4.DSbjpw.XP-NWs5z5WC0BnPySt2Zuc_XTEA')
 
-# Basic Bot was created by Habchy#1665
-# Please join this Discord server if you need help: https://discord.gg/FNNNgqb
-# Please modify the parts of the code where it asks you to. Example: The Prefix or The Bot Token
-# This is by no means a full bot, it's more of a starter to show you what the python language can do in Discord.
-# Thank you for using this and don't forget to star my repo on GitHub! [Repo Link: https://github.com/Habchy/BasicBot]
-
-# The help command is currently set to be Direct Messaged.
+# Cygnull A.I. Discord Bot was created by Cygnull#1348 
+# Please join this Discord server if you have any suggestions, comments, need help, or want to help out and become a Developer: https://discord.gg/hyGa7J2
+# Comments will be added as much as possible to help discern and explain code.
+#
+# # The help command is currently set to be Direct Messaged.
 # If you would like to change that, change "pm_help = True" to "pm_help = False" on line 9.
